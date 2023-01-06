60c214e6fdbb9dbc796cbfc13823e38e8b130d6a
GyoniDarvia <95419469+GyoniDarvia@users.noreply.github.com>
2023-01-07 01:49:59 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBtgBanEUJm6Q6cqsugpnUrB-frfll_T4Q",
+    authDomain: "signal-clone-4fe26.firebaseapp.com",
+    projectId: "signal-clone-4fe26",
+    storageBucket: "signal-clone-4fe26.appspot.com",
+    messagingSenderId: "1084661762769",
+    appId: "1:1084661762769:web:46b15b21eb129ee909d3f0"
+  };
